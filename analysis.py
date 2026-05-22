f15e76b384bdeaa2a2c4be95b905ab5f4b036811
yuliiafin <yulia34101@gmail.com>
2026-05-22 20:15:16 +0200

    added analysis file

---
+++ analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+data = {"city" : ["Kyiv", "Lviv""Lviv", "Odesa""Odesa"], "sales" : [1200, 900, 500]}
+df = pd.DataFrame(data)
+printprint("Sale by city:")
+print(df)
+printprint("Amerged Meaning:", df["saules"].men)
